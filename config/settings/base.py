594a794f0cb162e2b75aa0eeecffdff8bf35ed4a
maryus1991 <maryus19915123@gmail.com>
2025-04-12 22:24:20 +0330

    cofigure the main setting and split them

---
+++ config/settings/base.py
@@ -0,0 +1,120 @@
+from pathlib import Path
+
+from django.conf.global_settings import PASSWORD_HASHERS
+from dotenv import load_dotenv
+from os import getenv, path
+
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+
+APPS_DIR = BASE_DIR / "core_apps"
+
+local_env_file = path.join(BASE_DIR, ".envs", '.env.local')
+
+if path.isfile(local_env_file):
+    load_dotenv(dotenv_path=local_env_file)
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.humanize",
+
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "django_countries",
+    "django_filters",
+    "phonenumber_field",
+    "drf_spectacular",
+    "djoser",
+    "cloudinary",
+    "djcelery_email",
+    "django_celery_beat"
+]
+
+LOCAL_APPS = [
+    "core_apps.common",
+    "core_apps.user_profile",
+    "core_apps.user_auth",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(APPS_DIR / 'templates')],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = "/static/"
+STATIC_ROOT = str(APPS_DIR / "staticfiles")
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+SITE_ID = 1
---
+++ config/settings/local.py
@@ -0,0 +1,39 @@
+from os import getenv, path
+from dotenv import load_dotenv
+
+from .base import * # noqa
+from .base import BASE_DIR
+
+
+local_env_file = path.join(BASE_DIR, ".envs", '.env.local')
+if path.isfile(local_env_file):
+    load_dotenv(dotenv_path=local_env_file)
+
+
+# SECURITY WARNING: keep the secret key used in production.txt secret!
+SECRET_KEY = getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production.txt!
+DEBUG = getenv('DEBUG')
+
+ALLOWED_HOSTS = ['0.0.0.0']
+
+SITE_NAME = getenv('SITE_NAME')
+ADMIN_URL = getenv('ADMIN_URL')
+
+
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
+EMAIL_HOST = getenv('EMAIL_HOST')
+EMAIL_PORT = getenv('EMAIL_PORT')
+DEFAULT_FROM_EMAIL = getenv('DEFAULT_FROM_EMAIL')
+DOMAIN = getenv('DOMAIN')
+
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
